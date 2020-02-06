9344ee7c34cb457c77cd13182d2f86d152e97ebc
Anthony Jung <58079426+junganthonyy@users.noreply.github.com>
2020-02-06 11:42:48 -0800

    Added Proxy Files

---
+++ server/index.js
@@ -0,0 +1,16 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+const morgan = require('morgan');
+const cors = require('cors');
+const path = require('path');
+
+const app = express();
+const port = 3000;
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({extended: true}));
+app.use(morgan('dev'));
+app.use(cors());
+app.use(express.static(path.join(__dirname, '../client/dist')));
+
+app.listen(port, () => console.log(`\u001b[1;34m Server listening on port ${port}`));
